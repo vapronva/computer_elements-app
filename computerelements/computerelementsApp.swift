eada69b454ee5b038e89bc694a2b5d6aea56264e
vapronva <vparonsbybleep@gmail.com>
2021-03-21 00:15:36 +0600

    Initial commit

---
+++ computerelements/computerelementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  computerelementsApp.swift
+//  computerelements
+//
+//  Created by Vladimir Malinovskiy on 21.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct computerelementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
